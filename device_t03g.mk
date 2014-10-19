90bbe6d6c5a7c92f42c3dce7eb69eb551dc2e590
tilaksidduram <tilaksidduram@gmail.com>
2014-10-19 12:16:21 +0530

    Updates for multirom including touchscreen fix & MicroSD stuff

--- device_t03g.mk
+++ device_t03g.mk
@@ -21,13 +21,12 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/recovery/poweroff.sh:root/sbin/poweroff.sh \
         $(LOCAL_PATH)/modules/exfat_fs.ko:root/system/lib/modules/exfat_fs.ko \
         $(LOCAL_PATH)/modules/exfat_core.ko:root/system/lib/modules/exfat_core.ko \
-        $(LOCAL_PATH)/recovery/encryption/libkeyutils.so:root/sbin/libkeyutils.so \
-        $(LOCAL_PATH)/recovery/encryption/libsec_ecryptfs.so:root/sbin/libsec_ecryptfs.so \
-        $(LOCAL_PATH)/recovery/encryption/libsec_km.so:root/sbin/libsec_km.so \
 		$(LOCAL_PATH)/recovery/runatboot.sh:root/sbin/runatboot.sh \
 		$(LOCAL_PATH)/file_contexts:recovery/root/prebuilt_file_contexts \
         $(LOCAL_PATH)/recovery/init.recovery.smdk4x12.rc:root/init.recovery.smdk4x12.rc
-
+#       $(LOCAL_PATH)/recovery/encryption/libkeyutils.so:root/sbin/libkeyutils.so \
+#       $(LOCAL_PATH)/recovery/encryption/libsec_ecryptfs.so:root/sbin/libsec_ecryptfs.so \
+#       $(LOCAL_PATH)/recovery/encryption/libsec_km.so:root/sbin/libsec_km.so \
 
 $(call inherit-product, build/target/product/full.mk)
 
--- multirom/mr_init_devices.c
+++ multirom/mr_init_devices.c
@@ -24,8 +24,11 @@ const char *mr_init_devices[] =
  "/sys/module/mmc_core",
  "/sys/module/mmcblk",
 
+ // Input stuff
  "/sys/devices/virtual/input*",
  "/sys/devices/virtual/misc/uinput",
+ "/sys/devices/platform/s3c2440-i2c.3/i2c-3/3-0048/input/input2*", // Touchscreen
+ "/sys/devices/platform/s3c2440-i2c.5/i2c-5/5-0056/input/input12*", // Touchscreen
  "/sys/devices/platform/gpio-keys.0/input*",
  "/sys/class/sec/sec_key*",
  "/sys/class/sec/sec_touchkey*",
@@ -34,13 +37,18 @@ const char *mr_init_devices[] =
  // for adb
  "/sys/class/tty/ptmx",
  "/sys/devices/platform/dw_mmc/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p13", //system
- "/sys/devices/platform/dw_mmc/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p12", // /cache
+ "/sys/devices/platform/dw_mmc/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p12", //cache
  "/sys/class/misc/android_adb",
  "/sys/class/android_usb/android0/f_adb",
  "/sys/bus/usb",
 
- // USB Drive is in here
+ // MicroSD card
+ "/sys/block/mmcblk1",
+ "/sys/devices/platform/dw_mmc*",
+
+ // USB Drive & USB-OTG is in here
  "/sys/module/ehci_hcd*",
+ "/sys/devices/platform/s5p-ehci*",
 
  NULL
 };
